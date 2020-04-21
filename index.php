b4da8184cbdcc99334e242e741c6f1eb7ef627d4
bryanhodges <32679705+bryanhodges@users.noreply.github.com>
2020-04-20 20:43:38 -0500

    Get the updated project going after moving to php.

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
 <?php
 
-//1525 Program 3
 
-//The best way to get a project done faster is to start sooner. -Jim Highsmith
-
-//The first 90 percent of the code accounts for the first 90 percent of the development time.
-//The remaining 10 percent of the code accounts for the other 90 percent of the development time. -Tom Cargill
 
 session_start();
 
